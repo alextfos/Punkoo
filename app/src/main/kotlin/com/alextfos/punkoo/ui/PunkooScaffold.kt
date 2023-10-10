16947b88b746ba69076bf22af47cbff98221091d
Alejandro Traver <alextfos@gmail.com>
2023-10-10 11:56:19 +0200

    add preview

--- app/src/main/kotlin/com/alextfos/punkoo/ui/PunkooScaffold.kt
+++ app/src/main/kotlin/com/alextfos/punkoo/ui/PunkooScaffold.kt
@@ -1,18 +1,22 @@
 package com.alextfos.punkoo.ui
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.FabPosition
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.alextfos.punkoo.R
+import com.alextfos.punkoo.styles.ui.theme.PunkooTheme
 import com.alextfos.punkoo.styles.ui.theme.Theme
+import com.alextfos.punkoo.styles.ui.theme.ThemeType
 
 @Composable
 fun PunkooScaffold(
@@ -34,6 +38,21 @@ fun PunkooScaffold(
                 .padding(it)
         ) {
             content()
+        }
+    }
+}
+
+@Composable
+@Preview
+fun PunkooScaffoldPreview() {
+    val scaffoldState = rememberScaffoldState()
+
+    PunkooTheme(ThemeType.LIGHT) {
+        PunkooScaffold(scaffoldState = scaffoldState) {
+            Text(
+                modifier = Modifier.fillMaxSize(),
+                text = "Test"
+            )
         }
     }
 }
