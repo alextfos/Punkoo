403c0cbf6b518e42bddd61ae8ce1607c66d50e97
Alejandro Traver <alextfos@gmail.com>
2023-10-10 09:00:43 +0200

    add string

--- app/src/main/kotlin/com/alextfos/punkoo/ui/PunkooScaffold.kt
+++ app/src/main/kotlin/com/alextfos/punkoo/ui/PunkooScaffold.kt
@@ -3,12 +3,15 @@ package com.alextfos.punkoo.ui
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.FabPosition
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.alextfos.punkoo.R
 import com.alextfos.punkoo.styles.ui.theme.Theme
 
 @Composable
@@ -19,7 +22,9 @@ fun PunkooScaffold(
     Scaffold(
         scaffoldState = scaffoldState,
         topBar = {
-             TopAppBar(title = { Text("Mi Aplicación") })
+             TopAppBar(
+                 title = { Text(stringResource(R.string.app_name)) }
+             )
         },
         backgroundColor = Theme.colors.background,
         floatingActionButtonPosition = FabPosition.Center
